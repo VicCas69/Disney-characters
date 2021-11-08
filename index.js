ec27804acfe2a0e62c959fbe21b4a6bb35442d02
victor castro <victor_castro_01@hotmail.com>
2021-11-07 21:04:32 -0500

    Rendering the data

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 function characters(){
     return fetch("http://api.disneyapi.dev/characters")
     .then(res=>res.json())
-    .then(data=>console.log(data))
+    .then(data=>data.data.forEach(c=>{
+        if(c.name.toLowerCase() === document.getElementById('search').value.toLowerCase()){
+            characterRender(c)
+        }
+    }))
 }
+
+function allCharacters(){
+    return fetch("http://api.disneyapi.dev/characters")
+    .then(res=>res.json())
+    .then(data=>data.data.forEach(c=>characterRender(c))
+)}
+
+function characterRender(character){
+   
+    let name = document.createElement('div');
+    let image = document.createElement('img');
+    let btn = document.createElement('button')
+    let div = document.createElement('div')
+
+    name.innerText = character.name;
+    image.src = character.imageUrl;
+    btn.innerText = 'Like'
+    btn.addEventListener('click', function(){
+        if(btn.innerText === 'Like'){
+            btn.innerText = 'Liked'
+        }else{
+            btn.innerText = 'Like'
+        }
+    })
+    
+    document.getElementById('character-container').appendChild(name)
+    document.getElementById('character-container').appendChild(image)
+    document.getElementById('character-container').appendChild(div)
+    document.getElementById('character-container').appendChild(btn)
+    
+}
+
+//characters()
+
